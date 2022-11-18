36efcf5261e48828b9f942cab2cfb6e85909e3e4
Kyozen-1 <skadi1268@gmail.com>
2022-11-18 07:40:52 +0700

    fixed

---
+++ resources/views/opd/laporan/tc-27.blade.php
@@ -0,0 +1,73 @@
+<style>
+    th {
+        border: 1px solid;
+    }
+    td {
+        border: 1px solid;
+    }
+</style>
+<table class="table table-striped table-bordered text-center">
+    <thead>
+        <tr>
+            <th rowspan="3">Tujuan</th>
+            <th rowspan="3">Sasaran</th>
+            <th rowspan="3">Kode</th>
+            <th rowspan="3">Proram dan Kegiatan</th>
+            <th rowspan="3">Indikator Kinerja Tujuan, Sasaran, Program (outcome) dan Kegiatan (output)</th>
+            <th colspan="14">Target Kinerja Program dan Kerangka Pendanaan</th>
+            <th rowspan="3">Unit Kerja Perangkat Daerah Penanggung-jawab </th>
+            <th rowspan="3">Lokasi</th>
+        </tr>
+        <tr>
+            <th colspan="2">Tahun - 1</th>
+            <th colspan="2">Tahun - 2</th>
+            <th colspan="2">Tahun - 3</th>
+            <th colspan="2">Tahun - 4</th>
+            <th colspan="2">Tahun - 5</th>
+            <th colspan="2">Tahun - 1</th>
+            <th colspan="2">Kondisi Kinerja pada akhir periode Renstra Perangkat Daerah</th>
+        </tr>
+        <tr>
+            <th>Target</th>
+            <th>Rp</th>
+            <th>Target</th>
+            <th>Rp</th>
+            <th>Target</th>
+            <th>Rp</th>
+            <th>Target</th>
+            <th>Rp</th>
+            <th>Target</th>
+            <th>Rp</th>
+            <th>Target</th>
+            <th>Rp</th>
+            <th>Target</th>
+            <th>Rp</th>
+        </tr>
+        <tr>
+            <th>(1)</th>
+            <th>(2)</th>
+            <th>(3)</th>
+            <th>(4)</th>
+            <th>(5)</th>
+            <th>(6)</th>
+            <th>(7)</th>
+            <th>(8)</th>
+            <th>(9)</th>
+            <th>(10)</th>
+            <th>(11)</th>
+            <th>(12)</th>
+            <th>(13)</th>
+            <th>(14)</th>
+            <th>(15)</th>
+            <th>(16)</th>
+            <th>(17)</th>
+            <th>(18)</th>
+            <th>(19)</th>
+            <th>(20)</th>
+            <th>(21)</th>
+        </tr>
+    </thead>
+    <tbody id="tbodyTc17">
+        {!! $tc_27 !!}
+    </tbody>
+</table>
